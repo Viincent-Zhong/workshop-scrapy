6bd79e4a3bb671082bbcfaa31d360b618625bb5f
Vincent Zhong <vincent.zhong@epitech.eu>
2022-04-19 16:25:35 +0200

    [ADD] part of part 2

--- workshop_scraping/workshop_scraping/settings.py
+++ workshop_scraping/workshop_scraping/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'workshop_scraping.spiders'
 #USER_AGENT = 'workshop_scraping (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- workshop_scraping/workshop_scraping/spiders/ecommerce.py
+++ workshop_scraping/workshop_scraping/spiders/ecommerce.py
@@ -1,27 +1,22 @@
+from urllib.request import Request
 import scrapy
-
-
 
 class EcommerceSpider(scrapy.Spider):
     name = 'ecommerce'
     allowed_domains = ['webscraper.io']
-
-    def start_requests(self):
-        start_urls = ['https://webscraper.io/test-sites/e-commerce/static']
-        for url in start_urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = ['https://webscraper.io/test-sites/e-commerce/static']
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
-    # def parse(self, response):
-    #     item = scrapy.Item()
-    #     item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
-    #     item['name'] = response.xpath('//td[@id="item_name"]/text()').get()
-    #     item['description'] = response.xpath('//td[@id="item_description"]/text()').get()
-    #     item['link_text'] = response.meta['link_text']
-    #     url = response.xpath('//td[@id="additional_data"]/@href').get()
-    #     return response.follow(url, self.parse_additional_page, cb_kwargs=dict(item=item))
+        yield {
+            'title': response.xpath('//title/text()').getall(),
+            'name' : response.css('name').xpath('@src').getall(),
+            'description' : response.xpath('//p[contains(@class, "description")]').getall(),
+            'id' : response.css('id').xpath('@src').getall(),
+            'current_url' : response.xpath('href').getall(),
+            'image' : response.css('img').xpath('@src').getall(),
+            'detail_link' : response.xpath("//link").getall(),
+            'price' : response.css('div').xpath('@src').getall(),
+            'old_price' : response.css('old_price').xpath('@src').getall(),
+            'availability' : response.css('availability').xpath('@src').getall(),
+            'flag' : response.css('flag').xpath('@src').getall()
+        }
